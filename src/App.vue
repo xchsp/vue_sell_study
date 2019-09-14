2dd0ae0931abfa599515f59e1faae0648cb4fe57
e <xchsp@sina.com>
2019-09-14 17:22:10 +0800

    first init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import VHeader from 'components/v-header/v-header'
+
+export default {
+  name: 'app',
+  components: {
+      VHeader
+  }
+}
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
